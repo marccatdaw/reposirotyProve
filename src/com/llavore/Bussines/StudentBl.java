8afc7feb616b6c78392a31abaa68b4fa786fa86a
marccatdaw <marccatdaw@gmail.com>
2021-02-23 19:59:44 +0100

    Implemented funcionality: add student in business layer. close #4

--- src/com/llavore/Bussines/StudentBl.java
+++ src/com/llavore/Bussines/StudentBl.java
@@ -1,11 +1,25 @@
 package com.llavore.Bussines;
 
+import java.time.LocalDate;
+
+import com.llavore.dao.StudentDao;
 import com.llavore.model.Student;
 
 public class StudentBl {
 
 	public boolean add(Student student) {
-		throw new UnsupportedOperationException();
+		StudentDao studentDao = new StudentDao();
+		student.setAge(this.calculateAgeFromDate(student.getDateOfBirth()));
+		return studentDao.add(student);
 	}
 
+	private Integer calculateAgeFromDate(LocalDate date) {
+		LocalDate timeNow = LocalDate.now();
+		int age = timeNow.getDayOfYear() - date.getDayOfYear();
+		if (date.getMonth().getValue() < timeNow.getMonth().getValue()
+				&& date.getDayOfMonth() < date.getDayOfMonth()) {
+			age -= 1;
+		}
+		return age;
+	}
 }
