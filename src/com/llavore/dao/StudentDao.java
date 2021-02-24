b5964eceb56e9ef66b49dd1b714d580dd627c336
marccatdaw <marccatdaw@gmail.com>
2021-02-24 08:01:27 +0100

    Implemented add student in Dao Layer

--- src/com/llavore/dao/StudentDao.java
+++ src/com/llavore/dao/StudentDao.java
@@ -1,11 +1,21 @@
 package com.llavore.dao;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import com.llavore.model.Student;
 
 public class StudentDao {
 
+	private static List<Student> students;
+
+	static {
+		students = new ArrayList<Student>();
+	}
+
 	public boolean add(Student student) {
-		throw new UnsupportedOperationException();
+		students.add(student);
+		return true;
 	}
 
 }
