7a289e53e1b197cbee51125798b1f5309fb7fe99
marccatdaw <marccatdaw@gmail.com>
2021-02-24 13:07:53 +0100

    Implemented methods in class StudentBl. close #7

--- src/com/llavore/Bussines/StudentBl.java
+++ src/com/llavore/Bussines/StudentBl.java
@@ -1,6 +1,7 @@
 package com.llavore.Bussines;
 
 import java.time.LocalDate;
+import java.time.Period;
 
 import com.llavore.dao.StudentDao;
 import com.llavore.model.Student;
@@ -8,18 +9,20 @@ import com.llavore.model.Student;
 public class StudentBl {
 
 	public boolean add(Student student) {
-		StudentDao studentDao = new StudentDao();
+		boolean resultAction = false;
+
 		student.setAge(this.calculateAgeFromDate(student.getDateOfBirth()));
-		return studentDao.add(student);
+
+		StudentDao studentDao = new StudentDao();
+		if (studentDao.add(student)) {
+			resultAction = studentDao.addStudentToFile(student);
+		}
+
+		return resultAction;
 	}
 
 	private Integer calculateAgeFromDate(LocalDate date) {
-		LocalDate timeNow = LocalDate.now();
-		int age = timeNow.getDayOfYear() - date.getDayOfYear();
-		if (date.getMonth().getValue() < timeNow.getMonth().getValue()
-				&& date.getDayOfMonth() < date.getDayOfMonth()) {
-			age -= 1;
-		}
-		return age;
+		Period age = Period.between(date, LocalDate.now());
+		return age.getYears();
 	}
 }
